6011e397b4db23da997a63ef0991c8e7986ecd9e
Paul <paulr@freelearn.tv>
2017-07-18 18:33:53 +1200

    Change driver to worker

--- test/controllers/workers_controller_test.js
+++ test/controllers/workers_controller_test.js
@@ -5,14 +5,14 @@ const app = require('../../app');
 
 const Worker = mongoose.model('worker');
 
-describe('Drivers controller', () => {
-  it('Post to /api/drivers creates a new driver', (done) => {
-    Driver.count().then(count => { //coount drivers for test comparison
+describe('Workers controller', () => {
+  it('Post to /api/workers creates a new worker', (done) => {
+    Worker.count().then(count => { //coount workers for test comparison
       request(app) //using supertest library
-        .post('/api/drivers')
+        .post('/api/workers')
         .send( { email: 'test@test.com' } ) //sending along data to test required email schema
         .end(() => {
-          Driver.count().then(newCount => { //compare count after test data sent to mongo
+          Worker.count().then(newCount => { //compare count after test data sent to mongo
             assert(count + 1 === newCount);
             done();
           });
@@ -20,58 +20,58 @@ describe('Drivers controller', () => {
     });
   });
 
-  it('PUT to /api/drivers/id edits an existing driver', (done) => {
-    //step 1 create a driver - test changing driving from false to true
-    const driver = new Driver({ email: 't1@t.com', driving: false });
-    //step 2 save and edit the new driver 'driving flag'
-    driver.save().then(() => {
+  it('PUT to /api/workers/id edits an existing worker', (done) => {
+    //step 1 create a worker - test changing driving from false to true
+    const worker = new Worker({ email: 't1@t.com', driving: false });
+    //step 2 save and edit the new worker 'driving flag'
+    worker.save().then(() => {
       request(app)
-        .put(`/api/drivers/${driver._id}`) //es6 syntax equivalent to ('/api/drivers/' + driver_id)
-        .send({ driving: true })
+        .put(`/api/workers/${worker._id}`) //es6 syntax equivalent to ('/api/workers/' + worker_id)
+        .send({ working: true })
         .end(() => {
-          // step 3 test to find driver in mongo where flag for driver_id is now true
-          Driver.findOne({ email: 't1@t.com' })
-            .then(driver => {
-              assert(driver.driving === true);
+          // step 3 test to find worker in mongo where flag for worker_id is now true
+          Worker.findOne({ email: 't1@t.com' })
+            .then(worker => {
+              assert(worker.working === true);
               done();
             });
         });
     });
   });
 
-  it('DELETE to /api/drivers/id deletes an existing driver', (done) => {
-    //test logic create driver, delete driver, then test assert no driver can be found
-    const driver = new Driver({ email: 'test2_delete@t.com' });
+  it('DELETE to /api/workers/id deletes an existing worker', (done) => {
+    //test logic create worker, delete worker, then test assert no worker can be found
+    const worker = new Worker({ email: 'test2_delete@t.com' });
 
-    //step 2 save and then delete the new driver
-    driver.save().then(() => {
+    //step 2 save and then delete the new worker
+    worker.save().then(() => {
       request(app)
-        .delete(`/api/drivers/${driver._id}`)
+        .delete(`/api/workers/${worker._id}`)
         .end(() => {
-          // step 3 test to find driver in mongo where flag for driver_id is now true
-          Driver.findOne({ email: 'test2_delete@t.com' })
-            .then(driver => {
-              assert(driver === null);
+          // step 3 test to find worker in mongo where flag for worker_id is now true
+          Worker.findOne({ email: 'test2_delete@t.com' })
+            .then(worker => {
+              assert(worker === null);
               done();
             });
         });
     });
   });
 
-  it('Get to /api/drivers finds drivers in a location', (done) => {
-      const seattleDriver = new Driver({
+  it('Get to /api/workers finds workers in a location', (done) => {
+      const seattleWorker = new Worker({
         email: 'seattle@test.com',
         geometry: { type: 'Point', coordinates: [-122.4759902, 47.6147628] }
       });
-      const miamiDriver = new Driver({
+      const miamiWorker = new Worker({
         email: 'miami@test.com',
         geometry: { type: 'Point', coordinates: [-80.2534507, 25.791581] }
       });
 
-      Promise.all([seattleDriver.save(), miamiDriver.save()])
+      Promise.all([seattleWorker.save(), miamiWorker.save()])
         .then(() => {
           request(app)
-            .get('/api/drivers?lng=-80&lat=25')
+            .get('/api/workers?lng=-80&lat=25')
             .end((err, response) => {
               console.log(response.body[0].dis); //to display geoNear distance from query point
               assert(response.body.length === 1);
